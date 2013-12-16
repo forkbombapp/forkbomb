925b215bc4b9b06204a39cf9465527ca0181fad8
pezholio <pezholio@gmail.com>
2013-12-16 20:52:42 +0000

    Add omniauth callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,25 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          
   devise :omniauthable, :omniauth_providers => [:github]
+  
+  def self.find_for_github_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                           provider:auth.provider,
+                           uid:auth.uid,
+                           email:auth.info.email,
+                           password:Devise.friendly_token[0,20]
+                           )
+    end
+    user
+  end
+  
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
