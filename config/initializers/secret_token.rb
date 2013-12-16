c5f34a1dfb63e5ef505ae2c7d1a0a1209dc92409
James Smith <james@floppy.org.uk>
2013-12-16 13:16:39 +0000

    basic rails 4 / ruby 2 framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forkbomb::Application.config.secret_key_base = 'ee5546cfad1bd6def6953a3bff60cbf772685c9419e91d0dec3ca50727f74d951a99ab48e69067a1318f58bca116a385e2773efe3bb42846dac4d833b6d5c73e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Forkbomb::Application.config.session_store :cookie_store, key: '_forkbomb_session'
