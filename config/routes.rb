dc60647cbdc863284df0650af615949457c45bae
pezholio <pezholio@gmail.com>
2013-12-16 20:52:54 +0000

    Add omniauth methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Forkbomb::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all your routes lay out with "rake routes".  
 
   # You can have the root of your site routed with "root"
   root 'application#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
