5e4f82e2ef5c4a0ba0ac5dacf5a697a775c24c61
pezholio <pezholio@gmail.com>
2013-12-17 10:45:16 +0000

    Remove redundant Devise stuff

---
+++ db/migrate/20131217104309_remove_redundant_devise_stuff.rb
@@ -0,0 +1,10 @@
+class RemoveRedundantDeviseStuff < ActiveRecord::Migration
+  def change
+    remove_column :users, :encrypted_password
+    remove_column :users, :reset_password_token
+    remove_column :users, :reset_password_sent_at
+    remove_column :users, :reset_password_token
+    remove_column :users, :reset_password_token
+    remove_column :users, :reset_password_token
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216173106) do
+ActiveRecord::Schema.define(version: 20131217104309) do
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.string   "email",               default: "", null: false
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",       default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -32,6 +29,5 @@ ActiveRecord::Schema.define(version: 20131216173106) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
