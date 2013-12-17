f946e77efb6b18bec53eef634532d2ed202deeb6
pezholio <pezholio@gmail.com>
2013-12-17 15:13:50 +0000

    Add repo routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Forkbomb::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#index'
   
+  resources :repos, :except => [:create, :new, :destroy]
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
