bdda6daeb42b9589d38ca60f75a107a18fd82ae1
James Smith <james@floppy.org.uk>
2013-12-17 14:10:38 +0000

    add pull requester job
    resolves #2

---
+++ lib/pull_requester.rb
@@ -0,0 +1,28 @@
+class PullRequester
+
+  def self.perform(user, repo_name)
+    # get repository details
+    repo = Rails.application.github.repos.get user, repo_name
+
+    if repo.parent
+      branch = repo.default_branch
+      upstream_owner = repo.parent.owner.login
+      upstream_branch = repo.parent.default_branch
+
+      begin
+        Rails.application.github.pulls.create(user, repo_name,
+          title: 'Upstream changes',
+          body: 'Automatically opened by http://forkbomb.io',
+          head: "#{upstream_owner}:#{upstream_branch}",
+          base: branch
+        )
+      rescue Github::Error::UnprocessableEntity => ex
+        # absorb errors caused by already-open PRs
+        raise unless ex.message.include?("A pull request already exists")
+      end
+
+    end
+
+  end
+
+end
