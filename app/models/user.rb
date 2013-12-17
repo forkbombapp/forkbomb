39f737fcfca64ae63fb2b2bdbd48f7f302ec0655
pezholio <pezholio@gmail.com>
2013-12-17 10:40:12 +0000

    Remove database_recoverable etc from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         
+  devise :registerable, :rememberable, :trackable
   devise :omniauthable, :omniauth_providers => [:github]
   
   def self.find_for_github_oauth(auth, signed_in_resource=nil)
@@ -12,9 +10,8 @@ class User < ActiveRecord::Base
       user = User.create(name:auth.extra.raw_info.name,
                            provider:auth.provider,
                            uid:auth.uid,
-                           email:auth.info.email,
-                           password:Devise.friendly_token[0,20]
-                           )
+                           email:auth.info.email
+                        )
     end
     user
   end
