1c431fc779fa431b06374b324688c54d33a687f3
James Smith <james@floppy.org.uk>
2013-12-20 07:08:18 +0000

    Add user fork tests into a context
    To reduce cassette duplication

--- spec/models/fork_spec.rb
+++ spec/models/fork_spec.rb
@@ -1,27 +1,32 @@
 require 'spec_helper'
 
 describe Fork do
-  before(:each) do
-    @user = Rails.application.github.users.get user: 'pezholio'
-  end
   
-  it "should retrieve a list of forks", :vcr do
-    forks = Fork.get_for_user(@user)
-    forks.count.should == 8
-  end
+  context "with user" do
   
-  it "should fetch forks from the database if a cached version is present", :vcr do
-    Fork.get_for_user(@user)
-    Rails.application.github.should_not receive(:repos)
-    Fork.get_for_user(@user)
-  end
+    before(:each) do
+      @user = Rails.application.github.users.get user: 'pezholio'
+    end
   
-  it "should contact the API if the cache hasn't been updated in more than 1 day", :vcr do
-    Fork.get_for_user(@user)
-    Timecop.freeze(Date.today + 2) do
-      Rails.application.github.repos.should receive(:list).and_call_original 
+    it "should retrieve a list of forks", :vcr do
+      forks = Fork.get_for_user(@user)
+      forks.count.should == 8
+    end
+  
+    it "should fetch forks from the database if a cached version is present", :vcr do
+      Fork.get_for_user(@user)
+      Rails.application.github.should_not receive(:repos)
       Fork.get_for_user(@user)
     end
+  
+    it "should contact the API if the cache hasn't been updated in more than 1 day", :vcr do
+      Fork.get_for_user(@user)
+      Timecop.freeze(Date.today + 2) do
+        Rails.application.github.repos.should receive(:list).and_call_original 
+        Fork.get_for_user(@user)
+      end
+    end
+    
   end
   
   it "should generate a param", :vcr do 
